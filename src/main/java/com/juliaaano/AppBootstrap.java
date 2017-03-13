468bfc7f44f01e01f19cd6f2a42add5e4843344b
juliaaano <juliaaano@gmail.com>
2017-03-13 19:20:13 +1100

    Move print ascii banner to separate object

--- src/main/java/com/juliaaano/AppBootstrap.java
+++ src/main/java/com/juliaaano/AppBootstrap.java
@@ -4,10 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.slf4j.MDC;
 
-import java.io.BufferedReader;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.util.Optional;
+import static com.juliaaano.AsciiBanner.asciiBanner;
 
 public class AppBootstrap {
 
@@ -16,34 +13,6 @@ public class AppBootstrap {
     public static void main(String... args) {
 
         MDC.put("Correlation-Id", "correlation-id");
-        printApplicationAsciiBanner("application-ascii-banner.txt");
-    }
-
-    private static void printApplicationAsciiBanner(final String fileName) {
-
-        try {
-
-            final InputStream inputStream = getResourceAsStream(fileName)
-                    .orElseThrow(() -> new IllegalArgumentException("File " + fileName + " not in the classpath."));
-
-            final BufferedReader br = new BufferedReader(new InputStreamReader(inputStream));
-
-            String line;
-            while ((line = br.readLine()) != null) {
-                System.out.println(line);
-            }
-            br.close();
-
-        } catch (final Exception ex) {
-
-            logger.warn("Could not display application ascii banner.", ex);
-        }
-    }
-
-    private static Optional<InputStream> getResourceAsStream(final String fileName) {
-
-        return Optional.ofNullable(
-                AppBootstrap.class.getClassLoader().getResourceAsStream(fileName)
-        );
+        asciiBanner("application-ascii-banner.txt").ifPresent(AsciiBanner::print);
     }
 }
